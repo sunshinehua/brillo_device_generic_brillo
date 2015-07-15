73723a46517142bc9a7235faf1559e178f81be56
Nam T. Nguyen <namnguyen@google.com>
2015-07-15 13:04:40 -0700

    brilloemulator: Use Android emulator instead of qemu
    This CL adds an "emulator-arm" board to the BSP, and builds the
    brilloemulator product on top of this BSP.
    
    Two important changes in this CL are:
    
      * Including init.goldfish.rc
      * Including fstab.goldfish
    
    The second is not strictly required, it could be named anything, but the
    emulator searches for that file automatically. So it's nice to have.
    
    The init file is required though. The emulator is called "goldfish" so
    init will look for "init.goldfish.rc" first. We trampoline that to
    "init.brillo.rc" after setting some goldfish specific values.
    
    init.goldfish.sh is used to assign the emulator an IP address so that
    ADB can work.
    
    After building, use the following command to launch:
    
      emulator-arm -shell -no-window
    
    This CL does not remove the vexpressa9 platform yet though it no longer
    serves any purpose.
    
    Change-Id: I921ca2c8e709c1c260258399f8b37760dbff7d83

---
+++ bsp/boards/generic/emulator-arm/board.mk
@@ -0,0 +1,39 @@
+#
+# Copyright 2015 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Sample board config for use with Android's emulator (goldfish).
+$(call inherit-arch, arm)
+
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_KERNEL := true
+
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_SPARSE_EXT_DISABLED := true
+
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_FLASH_BLOCK_SIZE := 512
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 786432000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 576716800
+BOARD_CACHEIMAGE_PARTITION_SIZE := 69206016
+BOARD_SEPOLICY_DIRS += \
+	build/target/board/generic/sepolicy
+
+PRODUCT_COPY_FILES += \
+	device/generic/brillo/bsp/boards/generic/emulator-arm/init.goldfish.rc:root/init.goldfish.rc \
+	device/generic/brillo/bsp/boards/generic/emulator-arm/init.goldfish.sh:system/etc/init.goldfish.sh \
+	device/generic/brillo/bsp/boards/generic/emulator-arm/fstab.goldfish:root/fstab.goldfish \
+	device/generic/brillo/bsp/boards/generic/emulator-arm/ueventd.goldfish.rc:root/ueventd.goldfish.rc \
+	system/core/rootdir/ueventd.rc:root/ueventd.rc \
