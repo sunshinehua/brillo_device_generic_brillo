0ba05da45b6478dbf3f60757d516434308718010
Paul Stewart <pstew@google.com>
2015-08-05 10:26:09 -0700

    device/generic/brillo: Add WiFi firmware blobs for dragon 410c
    Include peripheral and device firmware for the WiFi part.
    
    BUG: 22976573
    TEST: Booted the dragon board
    TEST: echo 1 > /dev/wcnss_wlan
    TEST: echo sta > /sys/module/wlan/parameters/fwpath
    TEST: ifconfig wlan0 up
    TEST: iw wlan0 scan (with iw loaded via a separate step)
    
    Change-Id: I4c8beda68d3c48110e9583be2a15c106b5499bf8

--- bsp/boards/qcom/dragon_410c/board.mk
+++ bsp/boards/qcom/dragon_410c/board.mk
@@ -23,3 +23,25 @@ BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
 
 PRODUCT_COPY_FILES += \
   device/generic/brillo/bsp/boards/qcom/dragon_410c/fstab.brillo:root/fstab.brillo
+
+WLAN_FIRMWARE_SRC = device/generic/brillo/bsp/boards/qcom/firmware/wlan
+FIRMWARE_DST = system/vendor/firmware
+
+# Wireless peripheral firmware -- loaded by drivers/net/wireless/wcnss.
+WCNSS_FILES = mdt b00 b01 b02 b04 b06 b09 b10 b11
+WCNSS_COPY_FILES += \
+  $(join $(patsubst %, $(WLAN_FIRMWARE_SRC)/wcnss.%, $(WCNSS_FILES)), $(patsubst %, :$(FIRMWARE_DST)/wcnss.%, $(WCNSS_FILES)))
+
+PRODUCT_COPY_FILES += $(WCNSS_COPY_FILES)
+
+# WiFi driver -- loaded by drivers/staging/prima.
+WLAN_FIRMWARE_DST = $(FIRMWARE_DST)/wlan/prima
+PRIMA_FILES = \
+  WCNSS_cfg.dat \
+  WCNSS_qcom_cfg.ini \
+  WCNSS_qcom_wlan_nv.bin \
+  WCNSS_wlan_dictionary.dat
+PRIMA_COPY_FILES += \
+  $(join $(patsubst %, $(WLAN_FIRMWARE_SRC)/%, $(PRIMA_FILES)), $(patsubst %, :$(WLAN_FIRMWARE_DST)/%, $(PRIMA_FILES)))
+
+PRODUCT_COPY_FILES += $(PRIMA_COPY_FILES)
