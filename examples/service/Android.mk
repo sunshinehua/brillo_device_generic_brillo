214a1060c551e6944560dcd7048326e63102c420
Gilad Arnold <garnold@google.com>
2015-08-18 10:37:01 -0700

    Fix initrc generation example.
    Turns out that arbitrarily defined my_foo variables leak between
    targets. Instead, we should define them to as target-specific variables.
    
    Bug: 23313270
    Change-Id: I0a9f4feef85b2914745c947282eee38b7ef760fa

--- examples/service/Android.mk
+++ examples/service/Android.mk
@@ -32,9 +32,10 @@ LOCAL_MODULE_PATH := $(PRODUCT_OUT)/$(TARGET_COPY_OUT_INITRCD)
 
 include $(BUILD_SYSTEM)/base_rules.mk
 
-my_args := arg1 arg2
-my_groups := inet
 .PHONY: $(LOCAL_BUILT_MODULE)
+$(LOCAL_BUILT_MODULE): my_args := arg1 arg2
+$(LOCAL_BUILT_MODULE): my_groups := inet
 $(LOCAL_BUILT_MODULE): $(INITRC_TEMPLATE)
-	$(call generate-initrc-file,$(LOCAL_INIT_SERVICE),$(my_args),$(my_groups))
+	$(call generate-initrc-file,$(LOCAL_INIT_SERVICE),$(my_args),\
+		$(my_groups))
 endif
