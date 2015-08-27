64bd188e18b5f1ab8bd92dd5c62a8f159e927353
Lee Campbell <leecam@google.com>
2015-08-27 11:03:55 -0700

    brillo: Use system/etc/init for init scripts
    Remove the init.brillo.rc hack that was used to
    bootstrap our Brillo's init scripts.
    
    Now ro.hardware is defined by the SoC and
    all init scripts are installed in system/etc/init
    
    Change-Id: Ie7e76e792225e885a4bf96256f2f4e4d6b5f893e
    TEST: emulator boots and all brillo service are running
    BUG: 23588784

--- brillo_base.mk
+++ brillo_base.mk
@@ -19,12 +19,12 @@
 
 # Common Brillo init script
 PRODUCT_COPY_FILES += \
-  device/generic/brillo/init.brillo.rc:root/init.brillo.rc \
+  device/generic/brillo/init.brillo.rc:system/etc/init/init.brillo.rc \
   device/generic/brillo/init.firewall-setup.sh:system/etc/init.firewall-setup.sh \
   device/generic/brillo/init.set-sane-initial-date.sh:system/etc/init.set-sane-initial-date.sh
 
 # Directory for init files.
-TARGET_COPY_OUT_INITRCD := $(TARGET_COPY_OUT_ROOT)/initrc.d
+TARGET_COPY_OUT_INITRCD := $(TARGET_COPY_OUT_SYSTEM)/etc/init
 
 # Skip API checks.
 WITHOUT_CHECK_API := true
