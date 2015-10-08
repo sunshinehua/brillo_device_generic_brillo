f99492f8737ae991759928760b2391653f34825d
Tina Zhang <tina.zhang@intel.com>
2015-10-08 16:20:11 +0800

    Enable IA emulator
    This patch is used to enable building IA image. With this
    patch, one can generate the IA image and boot the emulator
    to shell.
    
    Build steps to generate system image are as follow:
    . build/envsetup.sh
    lunch brilloemulator_x86-eng
    make -jX
    
    To boot the system, please use brilloemulator-x86 or
    brilloemulator-x86_64 under base_product/qemu/ folder.
    
    To use adb feature, you need to download the patch:
    https://android-review.googlesource.com/#/c/175598/
    
    Change-Id: I36360ed616c41af0f440c7767721ba0fcb50ff44
    Signed-off-by: Bruce Beare <bruce.j.beare@intel.com>
    Signed-off-by: Tina Zhang <tina.zhang@intel.com>
    Signed-off-by: Lizhe Liu <lizhe.liu@intel.com>
    Signed-off-by: Jason Hu <jia-cheng.hu@intel.com>
    Signed-off-by: Yu Ning <yu.ning@intel.com>
    Original Author: Lizhe Liu <lizhe.liu@intel.com>

---
+++ brilloemulator_x86/bsp/qemu_x86.mk
@@ -0,0 +1,42 @@
+#
+# Copyright 2015 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# TODO(leecam): Make this automatic.
+LOCAL_PATH := device/generic/brillo/brilloemulator_x86/bsp
+
+TARGET_ARCH := x86
+TARGET_CPU_ABI := x86
+TARGET_KERNEL_ARCH := i386
+
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_KERNEL := false
+
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_FLASH_BLOCK_SIZE := 512
+TARGET_USERIMAGES_SPARSE_EXT_DISABLED := true
+
+PRODUCT_COPY_FILES += \
+  $(LOCAL_PATH)/init.qemu.rc:root/init.qemu.rc \
+  $(LOCAL_PATH)/initnetwork.sh:system/bin/initnetwork.sh \
+  system/core/rootdir/init.usb.rc:root/init.usb.rc \
+  system/core/rootdir/ueventd.rc:root/ueventd.rc \
+
+# Set up the local kernel.
+TARGET_KERNEL_SRC := hardware/bsp/kernel/common/android-3.18
+TARGET_KERNEL_DEFCONFIG := i386_defconfig
+TARGET_KERNEL_CONFIGS := $(realpath $(LOCAL_PATH)/brillo.kconf)
+
+BOARD_SEPOLICY_DIRS := $(BOARD_SEPOLICY_DIRS) $(LOCAL_PATH)/sepolicy
---
+++ brilloemulator_x86_64/BoardConfig.mk
@@ -0,0 +1,38 @@
+#
+# Copyright 2015 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Standard devices would usally define an SoC. As the emulator
+# has no SoC defnition we pull in a local qemu BSP.
+include device/generic/brillo/brilloemulator_x86_64/bsp/qemu_x86_64.mk
+
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 786432000
+# No ramdisk.
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 576716800
+
+# No ramdisk.
+BOARD_USES_FULL_RECOVERY_IMAGE := true
+
+TARGET_RECOVERY_FSTAB = device/generic/brillo/brilloemulator_x86_64/fstab.device
+
+# This is an emulator build.
+TARGET_SKIP_OTA_PACKAGE := true
+
+# Use clang.
+USE_CLANG_PLATFORM_BUILD := false
+
+PRODUCT_COPY_FILES += \
+    device/generic/brillo/brilloemulator_x86_64/fstab.device:root/fstab.device
