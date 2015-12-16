8692e2449e454ec33d8d6556c6f96547a5f93469
Mike Frysinger <vapier@google.com>
2015-12-15 19:03:30 -0500

    brilloemulator x86/x86_64: drop port 5555 hole punching
    Since we use the qemu pipe now on x86 targets, there's no need to open
    up port 5555.  We've also stopped forwarding the port on the qemu side.
    
    BUG=25434258
    TEST=booted emu and adb still works
    
    Change-Id: If58d897ee91b162e0fb8429634d738b04df6a60a

--- brilloemulator_x86_64/bsp/initnetwork.sh
+++ brilloemulator_x86_64/bsp/initnetwork.sh
@@ -3,5 +3,3 @@
 # Setup networking when boot starts
 ifconfig eth0 10.0.2.15 netmask 255.255.255.0 up
 route add default gw 10.0.2.2
-# Open up port 5555 for adb
-iptables -I INPUT -p tcp --dport 5555 -j ACCEPT -w
