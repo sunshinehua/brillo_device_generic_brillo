2e1b267ec12756d04ff9a8290fc50f0fb12498fd
Dan Shi <dshi@google.com>
2016-02-03 22:54:03 -0800

    Build test_suites package for Brillo
    This change enables brillo build to create Autotest package test_suites, which
    is required to run suite jobs in Autotest based on given build.
    
    Each board can specify its own list of test suites in AUTOTEST_TEST_SUITES.
    If there is no board specific setting of AUTOTEST_TEST_SUITES, all suite control
    files in autotest repo will be included.
    
    BUG: 26971750
    TEST: verified in local build
    
    Change-Id: I9acc60020c5d9b5e28344081e0a4c86776a54884

--- build/Android.mk
+++ build/Android.mk
@@ -20,12 +20,6 @@
 $(foreach f,$(HAL_MAKEFILES), \
     $(if $(wildcard $(f)),$(eval include $(f))))
 
-# The vendor partition package for brillo device.
-ifdef BRILLO_VENDOR_PARTITIONS
-
-# The staging directory to store vendor partitions.
-intermediates := $(call intermediates-dir-for, PACKAGING, vendor-partitions)
-
 ifneq "" "$(filter eng.%,$(BUILD_NUMBER))"
   # BUILD_NUMBER has a timestamp in it, which means that
   # it will change every time.  Pick a stable value.
@@ -38,6 +32,12 @@ name := $(TARGET_PRODUCT)
 ifeq ($(TARGET_BUILD_TYPE),debug)
   name := $(name)_debug
 endif
+
+# The vendor partition package for brillo device.
+ifdef BRILLO_VENDOR_PARTITIONS
+
+# The staging directory to store vendor partitions.
+intermediates := $(call intermediates-dir-for, PACKAGING, vendor-partitions)
 
 vendor_partition_zip := $(intermediates)/$(name)-vendor_partitions-$(FILE_NAME_TAG).zip
 
@@ -60,3 +60,42 @@ $(vendor_partition_zip) : $(vendor_partition_copied_files)
 $(call dist-for-goals, dist_files, $(vendor_partition_zip))
 
 endif
+
+AUTOTEST_DIR := external/autotest
+ifndef AUTOTEST_TEST_SUITES
+# If AUTOTEST_TEST_SUITES is not set in board config, include all suite control
+# files in autotest repo.
+TEST_SUITES_DIR := $(AUTOTEST_DIR)/test_suites
+AUTOTEST_TEST_SUITES := $(addprefix $(TEST_SUITES_DIR)/, $(call find-files-in-subdirs,$(TEST_SUITES_DIR),control.*,.))
+else  # AUTOTEST_TEST_SUITES is defined
+@echo "AUTOTEST_TEST_SUITES is already assigned:"
+@echo ${AUTOTEST_TEST_SUITES}
+endif  # ifndef AUTOTEST_TEST_SUITES
+
+# The staging directory to store test suites.
+intermediates := $(call intermediates-dir-for, PACKAGING, test_suites)
+target_test_suites_dir := $(intermediates)/autotest/test_suites
+
+test_suites_package := $(intermediates)/$(name)-test_suites-$(FILE_NAME_TAG).tar.bz2
+dependency_info := $(target_test_suites_dir)/dependency_info
+suite_to_control_file_map := $(target_test_suites_dir)/suite_to_control_file_map
+
+# Note that the test suites tar ball won't be regenerated if the depended
+# AUTOTEST_TEST_SUITES is not changed. For example, change in autotest module
+# control_file_preprocessor.py will not lead to the rebuild of the test suites
+# tar ball.
+$(test_suites_package) : $(AUTOTEST_TEST_SUITES)
+	@echo "Package test suites: $@"
+	# Build chromite site package in autotest
+	$(hide) $(AUTOTEST_DIR)/utils/build_externals.py chromiterepo
+	$(hide) rm -rf $(target_test_suites_dir)
+	$(hide) mkdir -p $(target_test_suites_dir)
+	$(foreach f, $(AUTOTEST_TEST_SUITES), \
+		$(shell cp $(f) $(target_test_suites_dir)/))
+	$(hide) python -B $(AUTOTEST_DIR)/site_utils/suite_preprocessor.py \
+		-a $(AUTOTEST_DIR) -o $(dependency_info)
+	$(hide) python -B $(AUTOTEST_DIR)/site_utils/control_file_preprocessor.py \
+		-a $(AUTOTEST_DIR) -o $(suite_to_control_file_map)
+	$(hide) cd $(dir $@) && tar -jcvf $(notdir $@) autotest
+
+$(call dist-for-goals, dist_files, $(test_suites_package))
