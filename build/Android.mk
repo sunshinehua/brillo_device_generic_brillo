722a95f8b461ed1cecb190eef4bffa36a9a75b41
Dan Shi <dshi@google.com>
2016-02-09 12:26:31 -0800

    Build autotest_server package for Brillo
    This change enables brillo build to create autotest_server_package, which
    is required to run test with server-side packaging. The package is the same
    for any board.
    
    Bug: 26971750
    Change-Id: I011d46315bd4b278e8a3e7398764be21cf5818e9
    TEST: verified in local build

--- build/Android.mk
+++ build/Android.mk
@@ -20,12 +20,6 @@
 $(foreach f,$(HAL_MAKEFILES), \
     $(if $(wildcard $(f)),$(eval include $(f))))
 
-# The vendor partition package for brillo device.
-ifdef BRILLO_VENDOR_PARTITIONS
-
-# The staging directory to store vendor partitions.
-intermediates := $(call intermediates-dir-for, PACKAGING, vendor-partitions)
-
 ifneq "" "$(filter eng.%,$(BUILD_NUMBER))"
   # BUILD_NUMBER has a timestamp in it, which means that
   # it will change every time.  Pick a stable value.
@@ -38,6 +32,12 @@ name := $(TARGET_PRODUCT)
 ifeq ($(TARGET_BUILD_TYPE),debug)
   name := $(name)_debug
 endif
+
+# The vendor partition package for brillo device.
+ifdef BRILLO_VENDOR_PARTITIONS
+
+# The staging directory to store vendor partitions.
+intermediates := $(call intermediates-dir-for, PACKAGING, vendor-partitions)
 
 vendor_partition_zip := $(intermediates)/$(name)-vendor_partitions-$(FILE_NAME_TAG).zip
 
@@ -60,3 +60,46 @@ $(vendor_partition_zip) : $(vendor_partition_copied_files)
 $(call dist-for-goals, dist_files, $(vendor_partition_zip))
 
 endif
+
+# Build autotest server side package.
+AUTOTEST_DIR := external/autotest
+autotest_server_pkg_intermediates := $(call intermediates-dir-for, PACKAGING, autotest_server_package)
+autotest_server_package := $(autotest_server_pkg_intermediates)/$(name)-autotest_server_package-$(FILE_NAME_TAG).tar.bz2
+
+# Exclude files not needed for autotest server-side packaging. The list of files
+# is different from what's used in building autotest server package in ChromeOS,
+# refer to https://chromium.googlesource.com/chromiumos/chromite/+/master/cbuildbot/commands.py#1997
+# The reason is that ChromeOS builder processes the autotest build output, which
+# does not include files not included in autotest ebuilds. Brillo builder needs
+# to filter the files from autotest repo, thus more excludes are needed here.
+exclude_dirs := .git \
+    apache \
+    cli \
+    client/build_deps \
+    client/deps \
+    client/profilers/*/*.tar.bz2 \
+    client/profilers/*/*.tar.gz \
+    client/site_tests \
+    client/tests \
+    contrib \
+    database \
+    docs \
+    logs \
+    packages \
+    puppylab \
+    results \
+    scheduler \
+    server/site_tests/native_Benchmarks \
+    site-packages \
+    tko
+
+excludes := $(addprefix --exclude , $(foreach d, $(exclude_dirs), $(d)))
+
+$(autotest_server_package) :
+	@echo "Package autotest server package: $@"
+	$(hide) rm -rf $(autotest_server_pkg_intermediates)/autotest
+	$(hide) rsync -r $(excludes) $(AUTOTEST_DIR) $(autotest_server_pkg_intermediates)
+	$(hide) tar -jcf $@ -C $(dir $@) autotest
+
+$(call dist-for-goals, dist_files, $(autotest_server_package))
+# End building autotest server side package.
