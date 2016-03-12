cf538438951f1df49ba238819b68caf24d29d3ba
Jorge Lucangeli Obes <jorgelo@google.com>
2016-03-11 16:36:09 -0800

    Make x86 emulators use the TCP transport.
    The QEMU prebuilts at
    prebuilts/android-emulator/linux-x86_64/qemu/linux-x86_64 seem to
    no longer work with ADB. Use the TCP transport until we figure out
    what's going on.
    
    Bug: 27486119
    Change-Id: I15d2c4303653e4c82ad9726cbf81d4f1fc878896

--- brilloemulator_x86_64/bsp/initnetwork.sh
+++ brilloemulator_x86_64/bsp/initnetwork.sh
@@ -1,5 +1,7 @@
 #!/system/bin/sh
 
-# Setup networking when boot starts
+# Set up networking when boot starts.
 ifconfig eth0 10.0.2.15 netmask 255.255.255.0 up
 route add default gw 10.0.2.2
+# Open up port 5555 for adb.
+iptables -I INPUT -p tcp --dport 5555 -j ACCEPT -w
